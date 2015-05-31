abe93d0d51ee563772b4cd04c2ec2e6f4707e23d
Juan Sebastian Vargas <juan_vargas10@hotmail.com>
2015-05-31 01:47:18 -0500

    Update arxiv.sh

--- Tareas/HW1/arxiv.sh
+++ Tareas/HW1/arxiv.sh
@@ -5,3 +5,5 @@ echo ===========================================================================
 #bajamos la pagina|buscamos las lineas que tienen Title:|quitams todo lo que esta antes de Title|para dejar solo el titulo |Buscamos la palabra
 curl http://arxiv.org/list/quant-ph/new | grep -iw Title:|sed 's/.*Title//g'|sed 's/.*span\>//g'|grep -iw $1
 #bajamos la pagina|buscamos las lineas que tienen Title:|quitams todo lo que esta antes de Title|para dejar solo el titulo |Buscamos la palabra
+echo numero de articulos encontrados:
+  curl http://arxiv.org/list/quant-ph/new | grep -iw Title:|sed 's/.*Title//g'|sed 's/.*span\>//g'|grep -iw $1| wc -l
