772c78074dbbce07f08fb41c4fad0f0cdbb8fd15
Juan Sebastian Vargas <juan_vargas10@hotmail.com>
2015-05-31 06:05:55 -0500

    punto 3 completo

--- Tareas/HW1/stardate.sh
+++ Tareas/HW1/stardate.sh
@@ -3,7 +3,9 @@ echo "#########################"
 echo StarDate
 echo "#########################"
 echo $1
-grep $1 worldhistory.tsv| awk -F"\t" '{print $2}'
+#se buscan los del ano
+awk -v var="$1" -F"\t" '{if($1==var) print $2}'worldhistory.tsv
 echo "#########################"
 echo "RA   DEC   HIP No."
+#se imprimen los 5 primeros que cumplen con condicion, y es estar dentro del 
 awk -v var="$1" -F"," '{if((2015-var)<$10&&(2016-var)>$10) print $8,$9,$2}' hyg.csv |head -5
