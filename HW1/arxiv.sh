87a8402710ffe840d95777a4da0d03961da546f9
Juan Sebastian Vargas <juan_vargas10@hotmail.com>
2015-05-30 23:32:06 -0500

    Sin comillas

--- HW1/arxiv.sh
+++ HW1/arxiv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "==========================================================================="
+echo ===========================================================================
 echo "Estas son los titulos para la palabra $1"
 echo "==========================================================================="
-
+#bajamos la pagina|buscamos las lineas que tienen Title:|quitams todo lo que esta antes de Title|para dejar solo el titulo |Buscamos la palabra
 curl http://arxiv.org/list/quant-ph/new | grep -iw Title:|sed 's/.*Title//g'|sed 's/.*span\>//g'|grep -iw $1
 #bajamos la pagina|buscamos las lineas que tienen Title:|quitams todo lo que esta antes de Title|para dejar solo el titulo |Buscamos la palabra
