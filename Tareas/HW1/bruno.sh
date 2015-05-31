d9aa0a0e983ebbdd0d184d4a45f61eac540472de
Juan Sebastian Vargas <juan_vargas10@hotmail.com>
2015-05-31 03:55:50 -0500

    Update bruno.sh

--- Tareas/HW1/bruno.sh
+++ Tareas/HW1/bruno.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "==============Punto (a)=============="
 echo el numero de estrellas es menos uno:
-wc -l kepler.csv
+sed "1d" kepler.csv|wc -l
 echo "==============Punto (b)=============="
 echo Estrellas con Masa menor a una centesima de la masa de Jupiter:
 awk -F, '{if($2>0&&$2<0.01)print $1}' kepler.csv 
@@ -10,6 +10,6 @@ awk -F, '{if($2>0&&$2<0.01)print $1}' kepler.csv |wc -l
 
 echo "==============Punto (c)=============="
 echo el planeta es:
-tail -4802 kepler.csv| sort -t"," -k6  |awk -F"," '{print $1}'|head -1
+sed "1d" kepler.csv| sort -t"," -k6  |awk -F"," '{print $1}'|head -1
 echo su periodo es:
-tail -4802 kepler.csv| sort -t"," -k6  |awk -F"," '{print $6}'|head -1
+sed "1d" kepler.csv| sort -t"," -k6  |awk -F"," '{print $6}'|head -1
