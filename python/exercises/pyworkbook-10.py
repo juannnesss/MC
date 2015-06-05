c48373a9217820d6294039ca4d0bc8a18f683553
Juan Sebastian Vargas <juan_vargas10@hotmail.com>
2015-06-05 12:03:20 -0500

    Primer punto python

---
+++ python/exercises/pyworkbook-10.py
@@ -0,0 +1,12 @@
+from math import log
+a=int(input("numero a"));
+b=int(input("numero b"));
+print("La suma es: " ,a+b);
+c=b-a
+print("La resta b-a es: ", c);
+print("La division:" ,a/b);
+print("El residuo:", a%b);
+print("El logaritmo en base 10 de a:", log(a,10));
+print("El resultado de a elevado a la b:",a**b);
+
+
