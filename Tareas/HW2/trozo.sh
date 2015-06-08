6a77ea4260ea7eddbb38b8237f19c447cb3bd35b
Juan Sebastian Vargas <juan_vargas10@hotmail.com>
2015-06-08 04:01:40 -0500

    trozo con comentarios

--- Tareas/HW2/trozo.sh
+++ Tareas/HW2/trozo.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+#calculo lo largo de la cadena
 final=$(echo "$2-$1+1"|bc -l)
+#guardo la cadena en una variable
 cadena=$(cat $3)
+#retorno la respuesta
 echo ${cadena:($1-1):$final}
