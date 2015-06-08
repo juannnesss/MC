b314e20267620111eef59d2ff8abd25e74a93ed2
Juan Sebastian Vargas <juan_vargas10@hotmail.com>
2015-06-08 04:08:54 -0500

    comentado

--- Tareas/HW2/flip.sh
+++ Tareas/HW2/flip.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 A=$1
 B=$2
+#como el remplazo es en los dos sentidos, se realizan los respectivos remplazos 
 cat $3 | sed 's/'$A'/1/g'| sed 's/'$B'/2/g'|sed 's/1/'$B'/g'|sed 's/2/'$A'/g'
