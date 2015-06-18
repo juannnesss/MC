70b1d8415ee012abf3667445e014bd1584c7afae
Juan Sebastian Vargas <juan_vargas10@hotmail.com>
2015-06-18 13:50:49 -0500

    experimento

--- python/exercises/pyworkbook-10.py
+++ python/exercises/pyworkbook-10.py
@@ -1,9 +1,9 @@
 from math import log
-a=int(input("numero a"));
-b=int(input("numero b"));
+a=int(input("numero a\n"));
+b=int(input("numero b\n"));
 print("La suma es: " ,a+b);
 c=b-a
-print("La resta b-a es: ", c);
+print("La resta b-a es:", c);
 print("La division:" ,a/b);
 print("El residuo:", a%b);
 print("El logaritmo en base 10 de a:", log(a,10));
